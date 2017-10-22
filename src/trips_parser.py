d3de7beda42d9cc6cfe7ec21f69636f18f790100
songsc <songscijnfan@hotmail.com>
2017-10-22 00:55:07 -0400

    Fixed the bug for missing last block

--- src/trips_parser.py
+++ src/trips_parser.py
@@ -27,18 +27,19 @@ def query(input_date, input_route):
     F_blocks = open(out_file, 'w')
     writer_blocks = csv.writer(F_blocks)
     
-    blocks = []
-    temp_block = b.Block()
+    blocks = []  # List of blocks
+    temp_block = b.Block() # A single block element
     
     trip = next(reader_trips)
     times1 = next(reader_times) # See below for explanation.
-    #times2 = times1
-    writer_blocks.writerow([trip[6], trip[2], trip[3], times1[2], times1[1]])
+    writer_blocks.writerow([trip[6], trip[2], trip[3], times1[2], times1[1]])  # Header
     times1 = next(reader_times)
             
     trip_count = 0
     for trip in reader_trips:        
+        # Move to the selected date 
         if input_date != "" and trip[1] != input_date: continue
+        
         trip_count += 1
         
         while times1[0] != trip[2]:
@@ -61,12 +62,14 @@ def query(input_date, input_route):
                 break                
         depart = times2[1]
         
-        if temp_block.get_bid() == "" or temp_block.get_bid() == trip[6]:
+        if temp_block.get_bid() == "null" or temp_block.get_bid() == trip[6]:
             temp_block.add_trip(trip[6], trip[2], trip[3], depart, arrival)
         else:
             blocks.insert(0, temp_block)
             temp_block = b.Block()
             temp_block.add_trip(trip[6], trip[2], trip[3], depart, arrival)
+    # Make sure to add the last block        
+    blocks.insert(0, temp_block)
         
     for block in blocks:
         block.write_block(writer_blocks)
