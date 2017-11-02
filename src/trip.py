64c759877c3b46da34668d9bbfa6a11789e9cf0a
songsc <songscijnfan@hotmail.com>
2017-11-01 21:31:44 -0400

    Added trip times, shapes

--- src/trip.py
+++ src/trip.py
@@ -4,18 +4,31 @@
 
 class Trip:
     
-    def __init__(self, bid, tid, headsign, depart, arrival):
+    def __init__(self, bid, tid, direct, headsign, origin, depart, destin, arrival, shape):
         self.bid = bid
         self.tid = tid
+        self.direct = direct
         self.headsign = headsign
+        self.origin = origin
         self.depart = depart
+        self.destin = destin
         self.arrival = arrival
+        self.shape = shape
         
     def print_trip(self):
-        print("%s, %s, %s, %s, %s" %(self.bid, self.tid, self.headsign, self.depart, self.arrival))
+        print("%s, %s, %s, %s, %s, %s, %s, %s, %s" 
+            %(self.bid, self.tid, 
+              self.direct, self.headsign, 
+              self.origin, self.depart, 
+              self.destin, self.arrival,
+              self.shape))
     
     def write_trip(self, writer):
-        writer.writerow([self.bid, self.tid, self.headsign, self.depart, self.arrival])
+        writer.writerow([self.bid, self.tid, 
+                         self.direct, self.headsign, 
+                         self.origin, self.depart, 
+                         self.destin, self.arrival,
+                         self.shape])
 
 if __name__ == "__main__":
-    print("Hello World")
+    print("This is the file for Class Trip. Run \"google_transit.py\" instead.")
